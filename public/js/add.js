5df0f77398039b49abd0838152fd5f3393fd8287
yodialfariz <yodialfariz@gmail.com>
2024-02-16 18:22:13 +0700

    update

--- public/js/add.js
+++ public/js/add.js
@@ -416,10 +416,10 @@ $(document).ready(function () {
     let total = ongkir - totalDiskon + surat + asuransi;
 
     // Format total menggunakan toLocaleString()
-    let formattedTotal = total.toLocaleString('id-ID', { minimumFractionDigits: 0 });
+    // let formattedTotal = total.toLocaleString('id-ID', { minimumFractionDigits: 3 });
 
     // Update nilai total untuk elemen yang sesuai
-    $('#total_harga').val(formattedTotal);
+    $('#total_harga').val(total);
   });
 
 
